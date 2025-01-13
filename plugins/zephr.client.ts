5d7bbf8d18490c976620d13083ead229877c2eb1
awelcing-alm <155472943+awelcing-alm@users.noreply.github.com>
2025-01-13 17:59:22 +0000

    zephr plugin

---
+++ plugins/zephr.client.ts
@@ -0,0 +1,16 @@
+export default defineNuxtPlugin(() => {
+    if (process.client) {
+      // Load the Zephr browser library if it hasn't been loaded already
+      if (!window.zephrBrowser) {
+        const zephrScript = document.createElement('script');
+        zephrScript.src = 'https://assets.zephr.com/zephr-browser/1.9.1/zephr-browser.umd.js';
+        zephrScript.async = true;
+        document.head.appendChild(zephrScript);
+  
+        zephrScript.onload = () => {
+          console.log('Zephr Browser loaded.');
+        };
+      }
+    }
+  });
+  
