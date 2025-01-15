97f76315bf284bee9f584a4ca8a1800076576e83
awelcing-alm <155472943+awelcing-alm@users.noreply.github.com>
2025-01-15 16:54:04 +0000

    move logic from plugin to app

--- plugins/zephr.client.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(() => {
-  if (typeof window !== 'undefined') {
-    const script = document.createElement('script');
-    script.src = 'https://assets.zephr.com/zephr-browser/1.9.1/zephr-browser.umd.js';
-    script.id = 'zephr-script';
-    script.onload = () => console.log('Zephr Browser loaded.');
-    document.head.appendChild(script);
-  }
-});
